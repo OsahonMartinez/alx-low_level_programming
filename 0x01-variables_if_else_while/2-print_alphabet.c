1f4482891139bb25f16897722fdade4c0a156227
OsahonMartinez <osahonmartins36@gmail.com>
2022-03-12 05:50:52 -0800

    Printing the alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return :Always 0 (success/correct)
+ */
+int main(void)
+{
+	char alpa;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+
+	putchat('\n');
+
+	return (0);
+}
+
