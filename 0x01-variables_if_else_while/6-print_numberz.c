416a923e79e95c6830ca50895feffb5fb668eb4f
OsahonMartinez <osahonmartins36@gmail.com>
2022-03-13 20:46:53 -0700

    Print single numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,13 +2,13 @@
 /**
  * main - Entry point
  *
- * Return : Always 0 (success/correct)
+ * Return: Always 0 (success/correct)
  */
 int main(void)
 
 {
 	int i;
-	
+
 	for (i = 48 ; i <= 57; i++)
 	{
 		putchar(i);
