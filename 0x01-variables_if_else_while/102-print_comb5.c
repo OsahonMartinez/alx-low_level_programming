f3afaf969f2935eaaa80cbd1bfdb3cbefd41fb21
OsahonMartinez <osahonmartins36@gmail.com>
2022-03-14 07:22:30 -0700

    task 12 complete

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+/**
+ * main - Print combination of 2-2 digits
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+
+{
+	int c, i, j, k;
+
+	for (c = 48; c <= 57; c++)
+	{
+		for (i = 48; i <= 57; i++)
+		{
+			for (j = 48; j <= 57; j++)
+			{
+				for (k = 48; k <= 57; k++)
+				{
+					if (((k + j) > (c + i) && k >= c) || c < k)
+					{
+						putchar(c);
+						putchar(i);
+						putchar(' ');
+						putchar(j);
+						putchar(k);
+
+						if (c + i + j + k == 227 && c == 57)
+						{
+							break;
+						}
+						else
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
