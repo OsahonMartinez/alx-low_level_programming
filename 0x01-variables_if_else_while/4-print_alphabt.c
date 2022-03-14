7531143bf37d94bbdbf3fd2c05a32eaf30daf961
OsahonMartinez <osahonmartins36@gmail.com>
2022-03-13 19:57:51 -0700

    print alphabt except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(success/correct)
+ */
+int main(void)
+{
+char alphabt;
+for (alphabt = 'a'; alphabt <= 'z'; alphabt++)
+{
+	if (alphabt == 'q')
+		continue;
+
+	else if (alphabt == 'e')
+		continue;
+
+	putchar(alphabt);
+}
+
+putchat('\n');
+
+return (0);
+}
+
