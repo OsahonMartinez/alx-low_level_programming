d4af546f7990de5f12a3f9b122e67d4ccd798742
OsahonMartinez <osahonmartins36@gmail.com>
2022-03-16 11:07:33 -0700

    print alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabets_x10 - Print alphabets
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	int alphabet;
+	int count;
+
+	count = 0;
+	while (count < 0)
+	{
+		for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+
+		count++;
+		_putchar('\n');
+	}
+
+}
