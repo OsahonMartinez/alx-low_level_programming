795500739b4afdab1914fe1c096fe6136f9badb7
OsahonMartinez <osahonmartins36@gmail.com>
2022-03-24 19:11:10 -0700

    task 8 complete

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,8 +7,7 @@
  * Return: string
  */
 
-char *rot13(char *s)
-{
+char *rot13(char *s) {
 	int count, rotation;
 	char r1[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
 		     'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 
