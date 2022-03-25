695df1e137a80bc21aa7a1d420fe78a56bdf5cab
OsahonMartinez <osahonmartins36@gmail.com>
2022-03-24 17:14:20 -0700

    task 6 completed

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string
+ * Return: Char value
+ */
+
+char cap_string(char *str)
+{
+	int a = 0, i;
+	int cspc = 13;
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
+	while (str[a])
+	{
+		i = 0;
+
+		while (i < cspc)
+		{
+			if ((a == 0 || str[a - 1] == spc[i]) && (str[a] >= 97 && str[a] <= 122))
+				str[a] -= 32;
+
+			i++;
+		}
+
+		a++;
+	}
+
+	return (str);
+}
