63214ee632a525f1fe482f2de20319aa3a235b16
OsahonMartinez <osahonmartins36@gmail.com>
2022-03-29 13:25:24 -0700

    Task 2 complete

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,7 +12,7 @@ char *_strchr(char *s, char c)
 	char *p; /* pointer to char */
 	int counter;
 
-	for (counter = 0; s[counter] != '\0'; counter++;)
+	for (counter = 0; s[counter] != '\0'; counter++)
 	{
 		if (s[counter] == c)
 		{
