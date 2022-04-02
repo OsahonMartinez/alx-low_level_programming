39962d58852a82f4c5ddf625f8b94fc7489e1d3f
OsahonMartinez <osahonmartins36@gmail.com>
2022-04-02 13:28:19 -0700

    Task 1 complete

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguments
+ * @argc: number of arguments
+ * @argv: array of the size
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	(void)argv;
+
+	for (i = 0; i < argc - 1; i++)
+	{
+		printf("argc = %d\n", argc);
+		return (0);
+	}
+}
